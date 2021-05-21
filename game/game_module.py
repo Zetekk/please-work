2d0195eec59b11c915ba6fed0c5f74a7b6018b9d
Zetekk <piotr.zapala@lo31.krakow.pl>
2021-05-21 11:05:44 +0200

    it works

--- game/game_module.py
+++ game/game_module.py
@@ -13,3 +13,13 @@ for file_name in file_names:
     image_name = file_name[:-4].upper()
     globals().__setitem__(image_name,
                           pygame.image.load(os.path.join(path, file_name)).convert_alpha(BACKGROUND))
+
+#grafika postaci
+PLAYER_WALK_LIST_R = [PLAYER_WALK1_R, PLAYER_WALK2_R, PLAYER_WALK3_R, PLAYER_WALK4_R,
+                      PLAYER_WALK5_R, PLAYER_WALK6_R, PLAYER_WALK7_R]
+
+PLAYER_WALK_LIST_L = [PLAYER_WALK1_L, PLAYER_WALK2_L, PLAYER_WALK3_L, PLAYER_WALK4_L,
+                      PLAYER_WALK5_L, PLAYER_WALK6_L, PLAYER_WALK7_L]
+
+#grafika platforma
+GRASS_LIST = [GRASS_SINGLE, GRASS_L, GRASS_C, GRASS_R]
--- game/main.py
+++ game/main.py
@@ -1,11 +1,17 @@
 import sys
+import pygame, os
+import game_module as gm
 
-import pygame
+os.environ['SDL_VIDEO_CENTERED'] = '1'          #centrowanie okna
+pygame.init()
 
 from pygame.locals import *
 pygame.init()
+
 pygame.display.set_caption('game base')
+
 screen = pygame.display.set_mode(gm.SIZESCREEN)
+
 clock = pygame.time.Clock()
 
 
@@ -77,4 +83,18 @@ while True:
 #big head
     #how it acts
 #ui
+#głowna pętla gra
+window_open = True
+while window_open:
+    #screen.fill(gm.LIGHTBLUE)
+    screen.blit(gm.BACKGROUND, (0,0))
+    #pętla zdarzeń
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            window_open = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                window_open = False
+
+        player.get_event(event)
     #pionts
