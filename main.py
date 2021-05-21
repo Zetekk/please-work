15715b9574b7c6726fc09d7eb16706100b6378f0
Zetekk <piotr.zapala@lo31.krakow.pl>
2021-05-21 10:31:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,74 @@
+import sys
 
+import pygame
 
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('game base')
 screen = pygame.display.set_mode(gm.SIZESCREEN)
 clock = pygame.time.Clock()
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, file_image):
-        super().__init__()
-        self.image = file_image
-        self.movement_x = 0
-        self.movement_y = 0
-        self._count = 0
-        self.rect = self.image.get_rect()
-        self.press_left = False
-        self.press_right = False
-        self.level = None
-        self.lifes = 3
-
-    def shoot(self):
-        if self.items.get("shotgun", False):
-            if self.rotate_left:
-                bullet = Bullet(gm.BULLET_L, self.rotate_left, self.rect.centerx, self.rect.centery)
-            else:
-                bullet = Bullet(gm.BULLET_R, self.rotate_left, self.rect.centerx, self.rect.centery)
-            self.level.set_of_bullets.add()
-
-
 #we need:
 #character
+#gamepad movement
+
+
+
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+for joystick in joysticks:
+    print(joystick.get_name())
+
+my_square = pygame.Rect(50, 50, 50, 50)
+my_square_color = 0
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+motion = [0, 0]
+
+while True:
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, colors[my_square_color], my_square)
+    if abs(motion[0]) < 0.1:
+        motion[0] = 0
+    if abs(motion[1]) < 0.1:
+        motion[1] = 0
+    my_square.x += motion[0] * 10
+    my_square.y += motion[1] * 10
+
+    for event in pygame.event.get():
+        if event.type == JOYBUTTONDOWN:
+            print(event)
+            if event.button == 0:
+                my_square_color = (my_square_color + 1) % len(colors)
+        if event.type == JOYBUTTONUP:
+            print(event)
+        if event.type == JOYAXISMOTION:
+            print(event)
+            if event.axis < 2:
+                motion[event.axis] = event.value
+        if event.type == JOYHATMOTION:
+            print(event)
+        if event.type == JOYDEVICEADDED:
+            joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+            for joystick in joysticks:
+                print(joystick.get_name())
+        if event.type == JOYDEVICEREMOVED:
+            joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
+#gamepad movement end
+
+
     #sprites
     #movement
     #guns and bullets
